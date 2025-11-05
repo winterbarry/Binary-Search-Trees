d6422ea8d5dc5b3bb4ce1e4fc41b3692a504d335
Obe Ajilima <ajilimaobe@gmail.com>
2025-11-05 13:22:18 +0100

    created node and tree classes

---
+++ script.js
@@ -0,0 +1,21 @@
+// 1
+class Node {
+    constructor(data, left, right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// 2
+class Tree {
+    constructor(array, root) {
+        this.array = array;
+        this.root = root;
+    }
+}
+
+// 3
+function buildTree(array) {
+    
+}
